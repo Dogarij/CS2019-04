715c762e85f5dba16a30109bf2695b7c4f0700ad
kirill <kirill.pliakhanau@gmail.com>
2019-04-09 20:13:01 +0300

    my taskc3  les4

--- src/kpliakhanau/lesson4/taskC3.java
+++ src/kpliakhanau/lesson4/taskC3.java
@@ -1,10 +1,19 @@
-package com.company.;
+package kpliakhanau.lesson4;
 
+import com.ibm.icu.text.RuleBasedNumberFormat;
 
-import java.util.Scanner;
+import java.util.Locale;
 
 public class taskC3 {
-    public static void main(String[] args){
 
+    public static void main(String[] args) {
+        RuleBasedNumberFormat nf = new RuleBasedNumberFormat(Locale.forLanguageTag("ru"), RuleBasedNumberFormat.SPELLOUT);
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(nf.format(i)+" умноженое на "+nf.format(j)+" равно "+nf.format(i*j));
+            }
+            System.out.println();
+        }
     }
 }
+
